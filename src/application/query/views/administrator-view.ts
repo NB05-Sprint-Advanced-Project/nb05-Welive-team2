85ca0e48305b88a8b24576cbe493e051f34ca05c
juho-creator <juhokim2018@gmail.com>
2026-01-09 10:29:04 +0900

    fix : 파일이름

--- src/application/query/views/administrator-view.ts
+++ src/application/query/views/administrator-view.ts
@@ -1,3 +1,5 @@
+import { Status } from "@prisma/client"
+
 export interface AdministratorView {
   data: {
     id: string;
@@ -18,3 +20,4 @@ export interface AdministratorView {
   limit: number;
   hasNext: boolean;
 }
+
