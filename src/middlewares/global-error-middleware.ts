c902abdf13748bf7fa4f2b959576ebb85e9cac45
juho-creator <juhokim2018@gmail.com>
2026-02-02 16:45:21 +0900

    [fix] bug

--- src/middlewares/global-error-middleware.ts
+++ src/middlewares/global-error-middleware.ts
@@ -1,11 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 import { getEnv } from '../config';
 import { isBusinessException } from '../shared/exception/business-exception/business-exception';
 import { isTechnicalException } from '../shared/exception/technical-exception/technical-exception';
 
 export const createGlobalErrorMiddleware = () => {
-  return (err: any, req: Request, res: Response) => {
+  return (err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
+    void next;
+
     const isDev = getEnv().NODE_ENV === 'development';
     if (isBusinessException(err)) {
       if (isDev) console.error(err);
